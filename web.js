414472cb18b81fe4f0de496a82c679d259950c74
Patrick Negri <patrick@iugu.com.br>
2013-07-11 23:50:26 -0300

    Trying again to read from index.html

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  buffer = fs.readFile("./index.html");
+  buffer = fs.readFileSync("./index.html");
   response.send( buffer.toString() );
 });
 
